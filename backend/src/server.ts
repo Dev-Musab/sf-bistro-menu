79bfdc9d1a5796775dced7f373960f535b50bd08
Dev-musab <Dev.tatatech@gmail.com>
2023-03-17 03:04:04 +0400

    updating service on backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,6 +21,20 @@ app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS, PUT"
+    );
+  
+    next();
+  });
+  
 app.use(express.static('public'));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'public', 'index.html'))
